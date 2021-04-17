3cd2917b45d8400138469a70866d6fa21dcc9e1c
Noparat Prasongdee <noparat.saimai@mail.kmutt.ac.th>
2021-04-17 22:02:29 +0700

    62130500126 [UPDATE] can add item to cart

--- src/components/ItemCard.vue
+++ src/components/ItemCard.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="item-card">
         <slot></slot>
-        <button class="absolute right-10 bottom-10 btn">Add to Cart</button>
     </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,6 +9,7 @@
             class="w-72 h-72"
           />
           <p class="absolute right-14 top-14">฿ {{ item.price }}</p>
+          <button class="absolute right-10 bottom-10 btn" @click="addToCart(item)">Add to Cart</button>
         </item-card>
       </div>
     </div>
@@ -24,6 +25,7 @@ export default {
     return {
       stocks: [],
       url: "http://localhost:5000/stocks",
+      cart_url: "http://localhost:5000/carts",
     };
   },
   methods: {
@@ -37,6 +39,23 @@ export default {
         console.log(`Could not get stocks! ${error}`);
       }
     },
+    async addToCart(item){
+      try{
+        await fetch(this.cart_url,{
+          method: "POST",
+          headers: {
+            "content-type":"application/json",
+          },
+          body: JSON.stringify({
+            name: item.name,
+            price: item.price,
+          }),
+        })
+        alert(`${item.name} added to Cart.`)
+      }catch(error){
+        console.log(`Could not add item! ${error}`)
+      }
+    }
   },
   async created() {
     this.stocks = await this.getStocks();
